49a257209bd07e43d06bd59a0a0d83ff4921d36e
Luiz Eduardo <oliveiraluiz.com@hotmail.com>
2020-06-12 03:38:56 -0300

    .SQL para adicionar os códigos no database

---
+++ SQL/AddSQLCode.sql
@@ -0,0 +1,7 @@
+--SELECT * FROM PROGRAMMING
+ 
+INSERT INTO PROGRAMMING(ID, ID_PROGRAMMER, ID_PROGRAMMING_LANGUAGE, KYU, TITLE, LINK, DATE, CODE) 
+VALUES (20, 2, 11, '8 kyu', 'Collect Tuition (SQL for Beginners #4)', 'https://www.codewars.com/kata/5910b0d378cc2ba91400000b', GETDATE(), '')
+
+--UPDATE PROGRAMMING SET LINK = 'https://www.codewars.com/kata/56c5847f27be2c3db20009c3' WHERE ID = 1
+
